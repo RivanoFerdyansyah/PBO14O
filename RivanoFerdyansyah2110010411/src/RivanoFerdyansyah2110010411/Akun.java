518ee339c649e1dc4d752c1e6beeef88f4cb9482
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-16 13:12:48 +0800

    Add files via upload

---
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Akun.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Akun extends Jurnal {
+    private String id,akun;
+    private int kdakun;
+    
+public Akun(){}
+
+    public void setIdAkun(String id){
+        this.id=id;
+    }
+    
+    public void setAkun(String akun){
+        this.akun=akun;
+    }
+    
+    
+    
+}
---
+++ RivanoFerdyansyah2110010411/src/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class User {
+    private String id,user, pass, level;
+    
+    public User (){}
+    
+    public void setIdUser(String id){
+        this.id=id;
+    }
+    
+    public String getIdUser(){
+        return this.id;
+    }
+    
+    public void setUser(String user){
+        this.user=user;
+    }
+    
+    public String getUser(){
+        return this.user;
+    }
+    
+    public void setPass(String pass){
+        this.pass=pass;
+    }
+    
+    public String getPass(){
+        return this.pass;
+}
+    public void setLevel(String level){
+        this.level=level;
+    }
+    
+    public String getLevel(){
+        return this.level;
+    }
+
+
+}
