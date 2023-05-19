4f850852484e860aa0de3fb588be3887b6beb3fa
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-20 00:34:37 +0800

    DetailJurnal.java

--- RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/DetailJurnal.java
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/DetailJurnal.java
@@ -7,6 +7,47 @@
  *
  * @author USER
  */
-public class DetailJurnal extends Jurnal {
+public class DetailJurnal {
+    private String id,ket, kduser, debit, kredit;
+    
+    public DetailJurnal (){}
+    
+    public void setNoJurnal(String id){
+        this.id=id;
+    }
+    
+    public String getNoJurnal(){
+        return this.id;
+    }
+    
+    public void setKeterangan(String ket){
+        this.ket=ket;
+    }
+    
+    public String getKeterangan(){
+        return this.ket;
+    }
+    
+    public void setKdUser(String kduser){
+        this.kduser=kduser;
+    }
+    
+    public String getKdUser(){
+        return this.kduser;
+    }
+    public void setDebit(String debit){
+        this.debit=debit;
+    }
+    
+    public String getDebit(){
+        return this.debit;
+    }
+    public void setKredit(String kredit){
+        this.kredit=kredit;
+    }
+    
+    public String getKredit(){
+        return this.kredit;
+    }
     
 }
