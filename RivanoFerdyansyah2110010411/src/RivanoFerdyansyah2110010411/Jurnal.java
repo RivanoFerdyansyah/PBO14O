3901ac99e789d0499530b1de89f026e30e69413f
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-20 00:35:55 +0800

    Jurnal.java

--- RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Jurnal.java
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Jurnal.java
@@ -7,6 +7,34 @@
  *
  * @author USER
  */
-public class Jurnal extends User{
+public class Jurnal {
+    
+    private String id,tgl, kduser;
+    
+    public Jurnal (){}
+    
+    public void setNoJurnal(String id){
+        this.id=id;
+    }
+    
+    public String getNoJurnal(){
+        return this.id;
+    }
+    
+    public void setTgl(String tgl){
+        this.tgl=tgl;
+    }
+    
+    public String getTgl(){
+        return this.tgl;
+    }
+    
+    public void setKdUser(String kduser){
+        this.kduser=kduser;
+    }
+    
+    public String getKdUser(){
+        return this.kduser;
+    }
     
 }
