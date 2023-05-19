2be20759d084b3686431456e82b0939f87dbbd79
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-20 00:36:22 +0800

    Produk.java

--- RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Produk.java
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Produk.java
@@ -7,6 +7,41 @@
  *
  * @author USER
  */
-public class Produk extends Transaksi{
+public class Produk {
+    
+    private String id,produk, harga, satuan;
+    
+    public Produk (){}
+    
+    public void setKdProduk(String id){
+        this.id=id;
+    }
+    
+    public String getKdProduk(){
+        return this.id;
+    }
+    
+    public void setNamaProduk(String produk){
+        this.produk=produk;
+    }
+    
+    public String getNamaProduk(){
+        return this.produk;
+    }
+    
+    public void setHarga(String harga){
+        this.harga=harga;
+    }
+    
+    public String getHarga(){
+        return this.harga;
+    }
+    public void setSatuan(String satuan){
+        this.satuan=satuan;
+    }
+    
+    public String getSatuan(){
+        return this.satuan;
+    }
     
 }
