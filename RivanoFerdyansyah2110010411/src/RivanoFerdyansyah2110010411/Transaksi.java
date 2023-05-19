d50bdedf630ec4085c89b1d1226c8bc51d4d6c77
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-20 00:36:45 +0800

    Transaksi.java

--- RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Transaksi.java
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/Transaksi.java
@@ -7,6 +7,49 @@
  *
  * @author USER
  */
-public class Transaksi extends User {
+public class Transaksi {
+    
+    private String id,tgl, kduser, bayar, kembali;
+    
+    public Transaksi (){}
+    
+    public void setNoFaktur(String id){
+        this.id=id;
+    }
+    
+    public String getNoFaktur(){
+        return this.id;
+    }
+    
+    public void setTgl(String tgl){
+        this.tgl=tgl;
+    }
+    
+    public String getTgl(){
+        return this.tgl;
+    }
+    
+    public void setKdUser(String kduser){
+        this.kduser=kduser;
+    }
+    
+    public String getKdUser(){
+        return this.kduser;
+    }
+    public void setBayar(String bayar){
+        this.bayar=bayar;
+    }
+    
+    public String getBayar(){
+        return this.bayar;
+    }
+    
+    public void setKembali(String kembali){
+        this.kembali=kembali;
+    }
+    
+    public String getKembali(){
+        return this.kembali;
+    }
     
 }
