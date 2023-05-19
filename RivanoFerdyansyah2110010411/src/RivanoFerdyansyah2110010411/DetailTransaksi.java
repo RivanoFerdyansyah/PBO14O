ceb0f9e09faf6ae373831bb7e6b29c24d5509dcf
RivanoFerdyansyah <120616741+RivanoFerdyansyah@users.noreply.github.com>
2023-05-20 00:35:02 +0800

    DetailTransaksi.java

--- RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/DetailTransaksi.java
+++ RivanoFerdyansyah2110010411/src/RivanoFerdyansyah2110010411/DetailTransaksi.java
@@ -7,6 +7,41 @@
  *
  * @author USER
  */
-public class DetailTransaksi extends Transaksi {
+public class DetailTransaksi {
+    
+    private String nofaktur,kdproduk, jml, total;
+    
+    public DetailTransaksi (){}
+    
+    public void setNofaktur(String nofaktur){
+        this.nofaktur=nofaktur;
+    }
+    
+    public String getNoFaktur(){
+        return this.nofaktur;
+    }
+    
+    public void setKdProduk(String kdproduk){
+        this.kdproduk=kdproduk;
+    }
+    
+    public String getProduk(){
+        return this.kdproduk;
+    }
+    
+    public void setJml(String jml){
+        this.jml=jml;
+    }
+    
+    public String getJml(){
+        return this.jml;
+    }
+    public void setTotal(String total){
+        this.total=total;
+    }
+    
+    public String getTotal(){
+        return this.total;
+    }
     
 }
